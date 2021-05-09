c26b4cd7048a0c22114753aa9f285821cf3ac2ef
westveerr <westveerr@hotmail.com>
2021-05-09 10:10:45 +0200

    Som aangepast

--- git_examples/count.py
+++ git_examples/count.py
@@ -2,7 +2,7 @@ def add_values(value1, value2):
     return value1 + value2
 
 def main():
-    print("Sum is: " + str(add_values(1, 3)))
+    print("Sum is: " + str(add_values(1, 5)))
 
 if __name__ == "__main__":
     main()
